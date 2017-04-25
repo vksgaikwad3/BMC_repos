3b8db8093a7da8f65bbaa352dd9af7ded5f4af2b
Vikas Gaikwad <vksgaikwad3@gmail.com>
2017-04-25 11:14:32 +0530

    included code for Jaypur site

---
+++ Jaypur/Saras_Chetavala_Jaypur/sim900.h
@@ -0,0 +1,43 @@
+#ifndef sim900_h
+#define sim900_h
+
+#include<Arduino.h>
+
+//600 --> NOT HTTP PDU
+//601 --> Network Error 
+//602 --> No Memory
+//603 --> DNS Error
+//604 --> Stack Busy
+    
+
+// SIM900 GSM/GPRS class
+
+class sim900_GPRS
+{
+  public: 
+
+    sim900_GPRS();     //Constructer
+    void power_on();  //Power on GSM method
+    int8_t sendATcommand(char* ATcommand, char* expected_answer, unsigned int timeout);
+    int8_t sendATcommand2(String ATcommand, char* expected_answer1,char* expected_answer2, unsigned int timeout);
+    int8_t sendATcommand3(String ATcommand, char* expected_answer1,char* expected_answer2,char*expected_answer3, unsigned int timeout);
+    void updateThinkSpeak(String channel_apiKey,uint8_t id1,uint8_t id2,uint8_t id3,uint8_t id4,float field1,float field2,float field3,float field4,float field5,float field6,float field7,float field8); //,float field6,float field7,float field8);
+    void updateMilkChillerRelays(String channel_apiKey,uint8_t field1,uint8_t field2,uint8_t field3,uint8_t field4,uint8_t field5,uint8_t field6,uint8_t field7,uint8_t field8);
+    float hextofloat(uint16_t hByte, uint16_t lByte); 
+    boolean httpGETupdate(String channelKey,float field1,float field2,float field3,float field4,float field5,float field6,float field7,float field8);  
+    void httpInit();
+    void  SendMessage();
+    void  verifyData(int conreq,int failreq,int sucessreq,float battemp,float milktemp);
+    void GPRSFailedSMS(String mobileNum,String message);
+    void GPRSFailedSMS();
+    char* Check_Operator();
+    
+  private:
+
+  
+   
+  
+};
+
+#endif
+
